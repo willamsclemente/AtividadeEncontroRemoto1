1385d86b8c80887255a704ad90b044b3a4e20275
willamsclemente <wila_clemente@hotmail.com>
2022-01-06 12:44:24 -0300

    Sistema de peças finalizado

---
+++ index.js
@@ -0,0 +1,24 @@
+let pesoPeca = 150
+
+if (pesoPeca > 100) {
+    console.log("Peço maior que 100g, podemos cadastrar a peça!")
+} else {
+    console.log("Peço insuficiente, a peça não pode ser cadastrada!")
+}
+
+let numeroPecas = 9
+
+if (numeroPecas < 10) {
+    console.log("Ainda há espaço na caixa, podemos cadastrar a peça!")
+} else {
+    console.log("Não há espaço disponível na caixa!")
+}
+
+let nomePeca = "Disco"
+
+if (nomePeca.length < 3) {
+    console.log("Nome da peça muito curto, não podemos cadastrar!")
+} else {
+    console.log("Nome da peça adequado, podemos cadastrar!")
+}
+
